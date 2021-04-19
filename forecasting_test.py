ef7c2efaad10953799a2c8ec9efa47a3a9d42544
kierensharma <kieren.sharma@gmail.com>
2021-04-19 13:41:19 +0100

    Added interpolated data

---
+++ forecasting_test.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+rcParams['figure.figsize'] = 16, 10
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+time = np.arange(0, 100, 0.1)
+sin = np.sin(time) + np.random.normal(scale=0.5, size=len(time))
+
+df = pd.DataFrame(dict(sine=sin), index=time, columns=['sine'])
+
+train_size = int(len(df) * 0.8)
+test_size = len(df) - train_size
+train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
+
+def create_dataset(X, y, time_steps=1):
+    Xs, ys = [], []
+    for i in range(len(X) - time_steps):
+        v = X.iloc[i:(i + time_steps)].values
+        Xs.append(v)
+        ys.append(y.iloc[i + time_steps])
+    return np.array(Xs), np.array(ys)
+
+time_steps = 10
+
+# reshape to [samples, time_steps, n_features]
+
+X_train, y_train = create_dataset(train, train.sine, time_steps)
+X_test, y_test = create_dataset(test, test.sine, time_steps)
+
+
+
