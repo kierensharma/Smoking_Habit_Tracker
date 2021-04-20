fa7e31fb83318897a881d533c4ad701b056f7e85
emilanz <kn18267@bristol.ac.uk>
2021-04-20 17:42:32 +0100

    modified noon2noon.py to interpolate, label and separate from noon to noon

--- label_handle.py
+++ label_handle.py
@@ -7,17 +7,38 @@ import os
 def main(label_path):
     for file in glob.glob(label_path):
         label_data = pd.read_csv(file)
-        hr_files = sorted(glob.glob('noon2noon_data/*'))
+        label_data['Sleep time'] = label_data['Day'].str.cat(label_data['Sleep time'],sep=" ")
+        label_data.drop('Day', inplace = True, axis = 1)
+        hr_files = sorted(glob.glob('noon2noon/interpolated/*'))
         for day in hr_files:
-            print(day[25:35])
+            print('DATE: ' + day[23:33])
             hr_data = pd.read_csv(day)
             hr_data['sleep time'] = int(0)
-            time = label_data.loc[label_data['Day'] == day[25:35], 'Sleep time'].to_frame()
-            for a in time.values:
-                for b in a:
-                    hr_data.loc[hr_data['Timestamp'].str.contains(str(b)), 'sleep time'] = int(1)
-            hr_data.to_csv('noon2noon_data/noon2noon_' + day[25:35] + '.csv')
-main('/Users/emiliolanzalaco/Documents/Smoking_Habit_Tracker/sleep_labels/sleep_labels.csv')
+            for x in label_data['Sleep time']:
+                print(pd.to_datetime(x).date)
+                # == hr_data['Timestamp']
+
+            # if hr_data['Timestamp'].Date == label_data['Sleep time'].Date:
+            #     print(true)
+            # time = label_data.loc[label_data['Sleep time'] == day[23:33], 'Sleep time'].to_frame()
+            # for a in time.values:
+            #     for b in a:
+            #         print(b)
+                    #hr_data.loc[hr_data['Timestamp'] > b, 'sleep time'] = int(1)
+            #print(hr_data)
+            #hr_data.to_csv('noon2noon_data/noon2noon_' + day[25:35] + '.csv')
+main('/Users/emiliolanzalaco/Smoking_Habit_Tracker/sleep_labels/feb_sleep_labels.csv')
+
+"""dt = pd.to_datetime("2016-11-13 22:01:25.450")
+print (dt)
+2016-11-13 22:01:25.450000
+
+print (df.index.get_loc(dt, method='nearest'))
+2
+
+idx = df.index[df.index.get_loc(dt, method='nearest')]
+print (idx)
+2016-11-13 22:00:28.417561344"""
 
 #df.loc[df[theme].isnull(), theme] = int(0)
 #, ['sleep time']] = int(1)
--- noon2noon.py
+++ noon2noon.py
@@ -19,20 +19,29 @@ def main(path):
     end_time = first_time[:11] + '11:59:59'
     first_12h = all_data.query("@first_time <= index <= @end_time")
     all_data = all_data.iloc[len(first_12h):] #this is cool
-
+    all_data['sleep time'] = int(0)
+    
+    all_data = all_data.reindex(all_data.resample('120s').asfreq().index, method='nearest',
+                        tolerance=pd.Timedelta('120s')).interpolate('time')
+    #appending sleep label
+    label_data = pd.read_csv('sleep_labels/feb_sleep_labels.csv')
+    label_data['Sleep time'] = label_data['Day'].str.cat(label_data['Sleep time'],sep=" ")
+    label_data.drop('Day', inplace = True, axis = 1)
+    for x in label_data['Sleep time']:
+        if pd.isnull(x) is False:
+            time = pd.to_datetime(x)
+            index = all_data.index.get_loc(time, method='nearest')
+            all_data['sleep time'].iloc[index] = int(1)
+            
+    
     #looping for all data
     while len(all_data) > 0:
         noon = str(all_data.index[0])
         next_day = str(int(noon[8:10]) + 1).zfill(2)
         noon_next_day = noon[:8] + next_day + ' 11:59:59'
         noon2noon = all_data.query("@first_time <= index <= @noon_next_day")
-        ts = noon2noon
-        ts = ts.reindex(ts.resample('120s').asfreq().index, method='nearest',
-                        tolerance=pd.Timedelta('120s')).interpolate('time')
-        all_data = all_data.iloc[len(ts):]
-      
-
-        ts.to_csv('noon2noon_' + noon[:10] + '.csv')
+        all_data = all_data.iloc[len(noon2noon):]
+        noon2noon.to_csv('/Users/emiliolanzalaco/Smoking_Habit_Tracker/noon2noon/labelled_interpolated/' + noon[:10] + '.csv')
 
 main('/Users/emiliolanzalaco/Documents/Smoking_Habit_Tracker/HR_CSV_Data/')
 
